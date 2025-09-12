b127958715c9efde8d6a684505e223742c8d02fd
Rinkame <maxlagoyda2006@gmail.com>
2025-09-12 13:49:20 +0300

    Create model Book

--- books/models.py
+++ books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField("Назва", max_length=255)
+    author = models.CharField("Автор", max_length=255)
+    genre = models.CharField("Жанр", max_length=100)
+    cover_image = models.URLField("Фото", blank=True, null=True)
+    year = models.PositiveIntegerField("Рік", blank=True, null=True)
+    description = models.TextField("Опис", blank=True)
+    country = models.CharField("Країна", max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.title} — {self.author}"
